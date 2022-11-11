54cf1f163b5c9f806ce2a32a9edcbfdc7156727e
blakelthaus <blakeborgholthaus@gmail.com>
2022-11-10 22:18:47 -0800

    add publisher logos and site logo

--- src/components/feed.jsx
+++ src/components/feed.jsx
@@ -2,12 +2,15 @@ import * as React from 'react';
 import { useEffect, useState } from 'react';
 import Box from '@mui/material/Box';
 import axios from 'axios';
-import { Link, Typography } from '@mui/material';
+import { Link } from '@mui/material';
+import CircularProgress from '@mui/material/CircularProgress';
 
 export default function Feed(props) {
 
   const type = props.data.type
   const title = props.data.title
+  const color = props.data.color
+  const extension = props.data.extension 
   const [links, setLinks] = useState()
   const [isLoading, setLoading] = useState(true)
  
@@ -35,44 +38,60 @@ export default function Feed(props) {
       return hours + 'h'
     }
   }
-
+  
   return (
-    <Box className="feed" 
-      sx={{ 
-        width: {
-          xs: '70%',
-          md: '27%'
-        },
-        marign: '5%',
-        padding: '30px',
-      }}
-    >
-      <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'left'}}>
-        {/* <Image sx={{ paddingRight: '10px' }}></Image> */}
-        <Typography variant="h6">{title}</Typography>
-      </Box>
-      
-      <Box className="links" sx={{
-        maxHeight: '350px',
-        overflow: 'scroll',
-        fontSize: '.9em'
-      }}>
+    <>
+      <Box className="feed" 
+        sx={{ 
+          width: {
+            xs: '70%',
+            md: '27%'
+          },
+          marign: '5%',
+          padding: '30px',
+        }}
+      >
+        <Box sx={{ height: '60px', backgroundColor: color, color: 'white', display: 'flex', alignItems: 'middle', padding: '10px', borderRadius: '10px' }}>
+          <Box sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', alignItems: 'middle' }}>
+            <img 
+              src={`/assets/${type}${extension}`}
+              alt={title}
+              style={{ width: '50px', padding: '10px'}}
+            />
+            {/* <Typography variant="h6">{title}</Typography> */}
+            <h3 style={{ height: '60px'}}>{title}</h3>
+          </Box>
+        </Box>
+        {isLoading && (
+          <Box sx={{ display: 'flex', justifyContent: 'center', paddingTop: '30px' }}>
+            <CircularProgress />
+          </Box>
+        )}
         {!isLoading && (
-          links.map(link => {
-            return (
-              <Box className="article" sx={{ display: 'flex', flexDirection: 'row', padding: '5px' }}>
-                <Box className="time" sx={{ color: 'grey', justifyContent: 'left', width: '30px', textAlign: 'left', paddingRight: '10px' }}>
-                  {getHoursSince(link.pubDate)}
-                </Box>
-                <Box className="title" >
-                  <Link target="_blank" href={link.link} sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', textAlign: 'left' }}>{link.title}</Link>
-                </Box>
-              </Box>
-            )
-          })
+          <Box className="links" sx={{
+            maxHeight: '350px',
+            overflow: 'scroll',
+            fontSize: '.9em'
+          }}>
+            {!isLoading && (
+              links.map(link => {
+                return (
+                  <Box className="article" sx={{ display: 'flex', flexDirection: 'row', padding: '5px' }}>
+                    <Box className="time" sx={{ color: 'grey', justifyContent: 'left', width: '30px', textAlign: 'left', paddingRight: '10px' }}>
+                      {getHoursSince(link.pubDate)}
+                    </Box>
+                    <Box className="title" >
+                      <Link target="_blank" href={link.link} sx={{ display: 'flex', flexDirection: 'row', justifyContent: 'left', textAlign: 'left' }}>{link.title}</Link>
+                    </Box>
+                  </Box>
+                )
+              })
+            )}
+          </Box>
         )}
       </Box>
-    </Box>
+    </>
+    
   )
 
 
--- src/components/header.jsx
+++ src/components/header.jsx
@@ -2,16 +2,13 @@ import * as React from 'react';
 import AppBar from '@mui/material/AppBar';
 import Box from '@mui/material/Box';
 import Toolbar from '@mui/material/Toolbar';
-import Typography from '@mui/material/Typography';
 
 export default function Header() {
   return (
     <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static" sx={{ backgroundColor: '#F7F7F7', boxShadow: 'none' }}>
         <Toolbar>
-          <Typography variant="h6" component="div" sx={{ color: 'black' }}>
-            MTBURLS
-          </Typography>
+          <img src="/assets/mtburls.png" alt="MTBURLS" style={{ height: '50px' }}/>
         </Toolbar>
       </AppBar>
     </Box>
